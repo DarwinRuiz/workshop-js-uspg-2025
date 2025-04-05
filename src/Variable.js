635764e638037d123a0e27f391b735bf2f173583
Darwin Ruiz <darwinorlandoruiz15@gmail.com>
2025-04-05 16:07:29 -0600

    repaso de variables

---
+++ src/Variable.js
@@ -0,0 +1,19 @@
+
+let nombre = "Ana"
+const edad = 25.5
+
+// desuso
+var esEstudiante = true
+
+//Tipos de datos
+/** 
+ * textos -> string, char
+ * numeros -> int, double, float -> (number)
+ * booleanos -> bool, boolean
+ *  
+ * 
+ * */
+
+console.log(typeof nombre)
+console.log(typeof edad)
+console.log(typeof esEstudiante)
